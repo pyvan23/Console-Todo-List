ad179b668de13912b0bd690dc254a879e2b6338b
pyvan23 <ivanmiguelalvarez@gmail.com>
2022-10-15 17:26:57 -0300

    pause function

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-import { pause, showMenu } from "./helpers/messages.js";
+import { menu, pause } from "./helpers/inquirer.js";
 
 console.clear();
-
-
-
 
 const main = async () => {
 
     let op = '';
 
     do {
-        op = await showMenu();
-        console.log({ op })
-        if (op !== '0') await pause();
 
+        op = await menu()
+        console.log({ op })
+        if(op !== '0') await pause()
+      
+        
     } while (op !== '0');
 
 };
--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -1,1 +1,64 @@
 import inquirer from 'inquirer';
+import color from "colors";
+
+const question = [{
+    type: 'list',
+    name: 'option',
+    message: 'what do you want to do?',
+    choices: [{
+        value: '1',
+        name: '1. Create todo'
+    }, {
+        value: '2',
+        name: '2. List Todos'
+    }, {
+        value: '3',
+        name: '3. List todos completed'
+    }, {
+        value: '4',
+        name: '4. List todos pending'
+    }, {
+        value: '5',
+        name: '5. Complete todos(s)'
+    }, {
+        value: '6',
+        name: '6. Delete todo'
+    }, {
+        value: '0',
+        name: '7. Exit'
+    }]
+}]
+
+
+
+
+export const menu = async () => {
+
+    console.clear()
+
+    console.log("==========================".green);
+    console.log("          Menu            ".green);
+    console.log("==========================\n".green);
+
+
+    const { option } = await inquirer.prompt(question)
+
+    return option;
+}
+
+export const pause = async () => {
+
+    const questionContinue = [{
+        type: 'input',
+        name: 'option',
+        message: 'Press ENTER to continue'.green,
+
+
+    }]
+    console.log('\n')
+    await inquirer.prompt(questionContinue)
+}
+
+
+
+
